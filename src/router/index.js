5df6a5856ded1410fefcb5c3b7a86c0ce6201d03
abdulmajid <majid@kebun.tech>
2023-08-26 02:33:59 +0700

    feat: add layout contact & signin-signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SigninPage from '../views/SigninPage.vue'
+import SignupPage from '../views/SignupPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SigninPage
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupPage
     }
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
   <section style="background-color: #FFF5E0;">
     <nav id="navbar" class="navbar navbar-expand-lg navbar-light fixed-top" style="transition: background-color 0.3s ease-in-out;">
       <div class="container">
-          <a class="navbar-brand" href="/home"><i class="bi bi-book pe-1" style="font-size: 30px;"></i> Library <span style="color: #d5802b; font-weight: 700;">Books</span></a>
+          <a class="navbar-brand" href="/"><i class="bi bi-book pe-1" style="font-size: 30px;"></i> Library <span style="color: #d5802b; font-weight: 700;">Books</span></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#home">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#about">About</a>
@@ -26,7 +26,9 @@
             </ul>
 
             <div class="d-flex">
-              <button class="btn rounded" style="background-color: #d5802b; color: white">Signin</button>
+              <router-link to="/signin">
+                <button class="btn rounded" style="background-color: #d5802b; color: white">Signin</button>
+              </router-link>
             </div>
           </div>
       </div>
@@ -58,24 +60,24 @@
   <section style="padding-bottom: 10rem; background-color: #fff;">
     <div class="container">
       <div class="row" style="margin-right: 0;">
-        <div class="col-md-3 text-center">
+        <div class="col-md-3 text-center" data-aos="fade-right">
           <div class="style-card"> 
             <h2>Novel</h2>
           </div>
         </div>
-        <div class="col-md-3 text-center">
+        <div class="col-md-3 text-center" data-aos="fade-down">
           <div class="style-card">
             <h2>
               Comic
             </h2>
           </div>
         </div>
-        <div class="col-md-3 text-center">
+        <div class="col-md-3 text-center" data-aos="fade-up">
           <div class="style-card">
             <h2>Technology</h2>
           </div>
         </div>  
-        <div class="col-md-3 text-center">
+        <div class="col-md-3 text-center" data-aos="fade-left">
           <div class="style-card">
             <h2>Kids</h2>
           </div>
@@ -84,14 +86,14 @@
     </div>
   </section>
 
-  <section>
+  <section id="about">
     <div class="row" style="margin-right: 0; height: 400px;">
-      <div class="col-md-7">
+      <div class="col-md-7" data-aos="fade-right">
         <div class="fixed-image">
           <img src="../assets/images/olesia-buyar-ZD03qVhBJZg-unsplash.jpg" alt="Gambar Tetap">
         </div>
       </div>
-      <div class="col-md-5 my-auto">
+      <div class="col-md-5 my-auto style-text-about" data-aos="fade-left">
         <h1>About Us</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem quidem beatae eius explicabo quibusdam debitis magnam sapiente. Laborum sint rerum praesentium blanditiis ea. Dolor vel aliquid eaque eos! Dolore facere provident maiores quae? Dolor excepturi quia debitis veritatis placeat numquam deleniti obcaecati nam accusamus eligendi vero perspiciatis, provident perferendis!</p>
       </div>
@@ -112,7 +114,7 @@
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L48,170.7C96,181,192,203,288,176C384,149,480,75,576,64C672,53,768,107,864,138.7C960,171,1056,181,1152,165.3C1248,149,1344,107,1392,85.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
   </section>
 
-  <section style="padding-top: 10rem; background-color: #fff;">
+  <section id="recommendation" style=" background-color: #fff;">
     <div class="container">
       <div class="row" style="margin-bottom: 5rem;">
         <div class="text-center">
@@ -157,11 +159,50 @@
             </div>
           </div>
         </div>      
-    </div>
+      </div>
+      
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFF5E0" fill-opacity="1" d="M0,128L48,133.3C96,139,192,149,288,133.3C384,117,480,75,576,64C672,53,768,75,864,74.7C960,75,1056,53,1152,48C1248,43,1344,53,1392,58.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    
+    <section id="contact" style="background-color: #FFF5E0">
+      <div class="container">
+
+        <div class="row" style="padding-bottom: 20px;"> 
+          <div class="col-md-5">
+            <h2>Library <span>Books</span></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, voluptates atque maxime quasi quos nesciunt. Consectetur laboriosam facere aliquam natus?</p>
+          </div>
+          <div class="col-md-3">
+            <h2>Social</h2>
+            <div class="style-social-contact">
+              <span><i class="bi bi-instagram"></i> Instagram</span>
+              <span><i class="bi bi-facebook"></i> Facebook</span>
+              <span><i class="bi bi-linkedin"></i> Linkedin</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <h2>Contact us</h2>
+            <div class="style-social-contact">
+              <span>+62 123123123123</span>
+              <span>test@mail.com</span>
+            </div>
+          </div>
+        </div>
+
+        <div style="width: 100%; height: 1px; background-color: black;"></div>
+        <div class="py-3">
+          <span>
+            copyright
+          </span>
+        </div>
+      </div>
   </section>
 </template>
 
 <script>
+  import AOS from 'aos';
+  // import 'aos/dist/aos.css';
+  AOS.init();
 
   document.addEventListener('DOMContentLoaded', function() {    
     const txtElement = ['jembatan ilmu untuk menghubungkan pengetahuan dengan kehidupan nyata.'];
@@ -283,10 +324,27 @@
       padding-left: 10px;      
   }
 
+  .style-social-contact {
+    display: flex;
+    flex-direction: column;
+  }
+
   @media only screen and (max-width: 768px) {
     #navbar {
-      background-color: #fff;
+      background-color: #fff;      
     }    
+    .fixed-image img {
+      display: flex;
+      width: 100%;
+      margin: 0 73px;
+    }
+    .style-text-about {
+      text-align: center;
+      font-size: 20px;      
+    }
+    .style-card-list {
+      padding-bottom: 10px;
+    }
   }
 
   @media only screen and (max-width: 600px) {
